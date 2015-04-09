7cfab9fb2ee4a5dda59611ea433aab2a61b974f0
ashraful88 <aaj.cse@gmail.com>
2015-04-09 16:49:46 +0600

    templates
    home view

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+from . import models
+
+class AdsIndex(generic.ListView):
+    queryset = models.Entry.objects.published()
+    template_name = "home.html"
+    paginate_by = 10
