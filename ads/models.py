88d796315964ab3fbfe047d52834e616d56d66e4
ashraful88 <aaj.cse@gmail.com>
2015-04-27 20:38:08 +0600

    user profile
    user profile

--- ads/models.py
+++ ads/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
 
 class Tag(models.Model):
 	slug = models.SlugField(max_length=200, unique=True)
@@ -49,3 +52,32 @@ class Entry(models.Model):
 		verbose_name = "Ad Entry"
 		verbose_name_plural = "Ad Entries"
 		ordering = ["-created"]
+
+#User Profile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+ 
+    def __str__(self):
+        return "{}'s profile".format(self.user.username)
+ 
+    class Meta:
+        db_table = 'user_profile'
+ 
+    def account_verified(self):
+        if self.user.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.user.email)
+            if len(result):
+                return result[0].verified
+        return False
+    
+    def profile_image_url(self):
+    	fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+ 
+    	if len(fb_uid):
+        	return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+
+
+ 
